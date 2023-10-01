aa790667bc8a1a50ef236dbc22809823b62a31a8
ival06001 <ival06001@E-PW024NVZ>
2023-10-01 14:40:58 +0200

    Add project files.

---
+++ Uppgift 3_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Uppgift_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            string val = Console.ReadLine();
+            int ålder = int.Parse(val);
+
+            if (ålder == 15)
+            {
+                Console.WriteLine("Du få komma in");
+            }
+
+            else if (ålder == 16)
+            {
+                Console.WriteLine("Du får komma in");
+            }
+
+            else if (ålder == 17)
+            {
+                Console.WriteLine("Du får komma in ");
+            }
+            
+            else if (ålder == 18)
+            {
+                Console.WriteLine("Du får komma in");
+            }
+
+            else if (ålder == 19)
+            {
+                Console.WriteLine("Du får komma in");
+            }
+
+            else
+            {
+                Console.WriteLine("Ajabaja, du är inte välkommen in, eftersom du inte är mellan 16-19 år gammal");
+
+            }
+        }
+    }
+}
